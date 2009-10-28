8bc627504dca01a5c495be80d45a31f4551c7546
Kim Altintop <kim.altintop@xing.com>
2009-10-28 16:24:26 +0100

    add teardown helper for gearmands, just in case

--- test/crash_test.rb
+++ test/crash_test.rb
@@ -3,19 +3,19 @@ require File.dirname(__FILE__) + '/test_helper'
 class CrashTest < Test::Unit::TestCase
 
   def teardown
-    system "killall gearmand"
+    teardown_gearmands
   end
 
   def test_worker_should_reconnect_if_gearmand_goes_away
-    system 'gearmand -d -p 4730'
+    start_gearmand
     worker = Gearman::Worker.new("localhost:4730", :reconnect_sec => 1)
     worker.add_ability("foo") {|data, job| "noop!" }
 
     response = nil
     EM.run do
       worker.work
-      system 'killall gearmand'
-      system 'gearmand -d -p 4730'
+      stop_gearmand
+      start_gearmand
 
       task = Gearman::Task.new("foo", "ping")
 
@@ -24,10 +24,12 @@ class CrashTest < Test::Unit::TestCase
     end
 
     assert_equal "noop!", response
+    stop_gearmand
   end
 
   def test_client_and_worker_should_use_failover_gearmand_if_primary_is_not_available
-    system 'gearmand -d -p 4731'
+    start_gearmand 4731
+
     worker = Gearman::Worker.new(["localhost:4730", "localhost:4731"], :reconnect_sec => 1)
     worker.add_ability("foo") {|data, job| "noop!" }
 
@@ -40,15 +42,13 @@ class CrashTest < Test::Unit::TestCase
     end
 
     assert_equal "noop!", response
+    stop_gearmand 4731
   end
 
   def test_client_and_worker_should_switch_to_failover_gearmand_if_primary_goes_down
-    pidfile = "/tmp/gearmand#{$$}.pid"
-    system "gearmand -d -p 4730 --pid-file=#{pidfile}"
-    gearmand_pid = `cat #{pidfile}`.to_i
-    File.unlink pidfile
+    start_gearmand 4730
+    start_gearmand 4731
 
-    system 'gearmand -d -p 4731'
     worker = Gearman::Worker.new(["localhost:4730", "localhost:4731"], :reconnect_sec => 1)
     worker.add_ability("foo") {|data, job| "noop!" }
 
@@ -56,7 +56,7 @@ class CrashTest < Test::Unit::TestCase
     EM.run do
       worker.work
 
-      Process.kill "KILL", gearmand_pid
+      stop_gearmand 4730
 
       task = Gearman::Task.new("foo", "ping")
       task.on_complete {|res| response = res }
@@ -64,5 +64,6 @@ class CrashTest < Test::Unit::TestCase
     end
 
     assert_equal "noop!", response
+    stop_gearmand 4731
   end
 end
